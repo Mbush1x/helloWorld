b1f8e6844991d190715ac29db8a040439826fb36
mbush1x <mbush1@umd.edu>
2024-03-28 17:11:16 -0400

    revised error in route for favorite course and contact.html

--- app.py
+++ app.py
@@ -17,17 +17,24 @@ def hello():
 def about():
     return render_template('about.html')
 
+
 @app.route('/favorite-course')
 def favorite_course():
-    print('Entered Subject Name: ' + request.args.get('subject'))
-    print('Entered Course Number: ' + request.args.get('course_number'))
-      return render_template('favorite-course.html')
+    subject = request.args.get('subject', '')
+    course_number = request.args.get('course_number', '')
+    print('Entered Subject Name: ' + subject)
+    print('Entered Course Number: ' + course_number)
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        print('Entered First Name: ' + request.form.get('firstName'))
+        print('Entered Last Name: ' + request.form.get('lastName'))
+        print('Entered Email Address: ' + request.form.get('email'))
+        return render_template('contact.html', submitted = True)
     return render_template('contact.html')
-
 
 
 if __name__ == '__main__':
